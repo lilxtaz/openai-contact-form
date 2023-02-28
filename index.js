91e1dc4da02bb2e1bc465d9f64f878b79003470c
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-02-28 22:44:02 +0100

    app commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const configuration = new Configuration({
+    organization: "org-ky6vlFY7VmfTrJnB21jZ79OF",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3080;
+
+app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+app.post('/', async function(req, res){
+
+    const { message } = req.body;
+
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `\n\n${message}`,
+        max_tokens: 3990,
+        temperature: 0.5,
+
+      });
+
+      res.json({
+
+        message: response.data.choices[0].text.trim()
+
+      })
+
+})
+
+app.listen(port, function(req, res){
+
+    console.log('Server is up and running on port 3080');
+
+})
