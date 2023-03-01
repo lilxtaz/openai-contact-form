1b528232274fb228e4e83446926da0d13a52fb45
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-03-01 01:57:02 +0100

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = 3080;
 app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
-    res.sendFile(path.resolve( 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
 app.post('/', async function(req, res){
